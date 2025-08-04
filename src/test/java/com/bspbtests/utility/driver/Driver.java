9922354f66c90f352658bb07983ef9d85c37a5b5
AenArMarie <vafailgvinbleid@gmail.com>
2025-08-04 15:58:45 +0300

    Added environmental variable for remote or local web driver initialization

--- src/test/java/com/bspbtests/utility/driver/Driver.java
+++ src/test/java/com/bspbtests/utility/driver/Driver.java
@@ -2,23 +2,58 @@ package com.bspbtests.utility.driver;
 
 import com.bspbtests.constants.PathConstants;
 import com.utility.files.FilesReader;
+import org.assertj.core.api.Assumptions;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.AbstractDriverOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
 public class Driver {
 
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
+    private static final Map<String, Supplier<AbstractDriverOptions<?>>> REMOTE_WEB_DRIVER_OPTIONS_FACTORY =
+            Map.of(
+                    "chrome", () -> {
+                        var opts = new ChromeOptions();
+                        opts.setCapability("browserVersion", "128.0");
+                        return opts;
+                    },
+                    "firefox", () -> {
+                        var opts = new FirefoxOptions();
+                        opts.setCapability("browserVersion", "125.0");
+                        return opts;
+                    }
+            );
 
+    private static final Map<String, Supplier<WebDriver>> LOCAL_WEB_DRIVER_FACTORY =
+            Map.of(
+                    "chrome", ChromeDriver::new,
+                    "firefox", FirefoxDriver::new
+            );
+
+    private static final HashMap<String, Object> SELENOID_OPTIONS =
+            new HashMap<>() {{
+                put("name", "Test badge...");
+                put("sessionTimeout", "15m");
+                put("env", new ArrayList<String>() {{
+                    add("TZ=UTC");
+                }});
+                put("labels", new HashMap<String, Object>() {{
+                    put("manual", "true");
+                }});
+                put("enableVideo", true);
+            }};
 
     public static WebDriver instance() {
         if (driver.get() == null) {
@@ -33,30 +68,22 @@ public class Driver {
 
     private static WebDriver createDriver() throws MalformedURLException {
         BrowserModel browser = FilesReader.readJson(PathConstants.BROWSER_CONFIG_PATH, BrowserModel.class);
-        AbstractDriverOptions options;
-        switch (browser.getName()) {
-            case "chrome" -> {
-                options = new ChromeOptions();
-                options.setCapability("browserVersion", "128.0");
-            }
-            case "firefox" -> {
-                options = new FirefoxOptions();
-                options.setCapability("browserVersion", "125.0");
-            }
-            default ->
-                throw new IllegalArgumentException("Неподдерживаемый браузер: " + browser.getName());
+        Assumptions.assumeThat(browser).isNotNull();
+        if (System.getenv("remoteDriver").equals("true"))
+            return createRemoteWebDriver(browser);
+        else
+            return LOCAL_WEB_DRIVER_FACTORY.get(browser.getName()).get();
+    }
+
+    private static WebDriver createRemoteWebDriver(BrowserModel browser) {
+        var supplier = REMOTE_WEB_DRIVER_OPTIONS_FACTORY.get(browser.getName().toLowerCase());
+        if (supplier == null) {
+            throw new IllegalArgumentException(
+                    "Неподдерживаемый браузер: " + browser.getName()
+            );
         }
-        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-            put("name", "Test badge...");
-            put("sessionTimeout", "15m");
-            put("env", new ArrayList<String>() {{
-                add("TZ=UTC");
-            }});
-            put("labels", new HashMap<String, Object>() {{
-                put("manual", "true");
-            }});
-            put("enableVideo", true);
-        }});
+        AbstractDriverOptions<?> options = supplier.get();
+        options.setCapability("selenoid:options", SELENOID_OPTIONS);
         return RemoteWebDriver.builder().
                 address(URI.create("http://localhost:4444/wd/hub")).
                 oneOf(options).
